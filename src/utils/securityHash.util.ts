6a4a564ab7f7824ef2e020020d74b1c9a45d7d13
romulo <rsrevan2000zsx@gmail.com>
2026-05-19 15:44:42 -0300

    feat: refatoração do fluxo de autenticação e otimização dos modelos

---
+++ src/utils/securityHash.util.ts
@@ -0,0 +1,13 @@
+import bcrypt from 'bcrypt';
+
+const SALT_ROUNDS = 10;
+
+export const SecurityHash = {
+  hashPassword: async (password: string): Promise<string> => {
+    return await bcrypt.hash(password, SALT_ROUNDS);
+  },
+
+  comparePassword: async (password: string, encryptedPassword: string): Promise<boolean> => {
+    return await bcrypt.compare(password, encryptedPassword);
+  }
+};
