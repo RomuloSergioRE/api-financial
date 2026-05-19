08237516f38d0d2603ef47c21937ff4a041e4fec
romulo <rsrevan2000zsx@gmail.com>
2026-05-19 09:08:50 -0300

    chore: initial project

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import type { Application, Request, Response } from 'express';
+import cors from 'cors';
+
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/health', (req: Request, res: Response) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Servidor rodando perfeitamente!',
+    timestamp: new Date().toISOString()
+  });
+});
+
+export default app;
